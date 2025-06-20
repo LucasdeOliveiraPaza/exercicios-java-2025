fc9e3787d1d3856c29697e2d52d422fe325cdbfa
lucas <lucasdeoliveirapaza@gmail.com>
2025-06-20 14:31:54 -0300

    finalizar exercicio 1

--- src/exercicios/Ex1.java
+++ src/exercicios/Ex1.java
@@ -1,5 +1,21 @@
 package exercicios;
 
-public class Ex1 {
+import java.util.Scanner;
 
+public class Ex1 {
+     public static void main(String[] args) {
+        int numero = 0;
+     
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("digite um numero");
+        numero = scan.nextInt();
+
+         System.out.printf(
+            "Você escolheu o número %d, que é antecessor do número %d e sucessor do número %d",
+            numero,
+            (numero + 1),
+            (numero - 1)
+        );
+     }
 }
